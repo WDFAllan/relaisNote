969caee26e7259c066c9e937906e591fc6dd04fd
WDFAllan <wdfallan@gmail.com>
2026-09-10 16:32:41 +0200

    Ajoute l autorisation JWT dans Swagger

--- src/Relais.Api/Program.cs
+++ src/Relais.Api/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Relais.Domain.Services;
 using Relais.Infrastructure.Persistence;
 
@@ -33,7 +34,30 @@ builder.Services.AddAuthorization();
 // --- API ---
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Saisir uniquement le JWT, sans le préfixe Bearer.",
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        [new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer",
+            },
+        }] = Array.Empty<string>(),
+    });
+});
 
 var app = builder.Build();
 
